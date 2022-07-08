fc0cf59fa33079eab24c215dfd42768a0cb81002
JackWorld <jackwong0326@gmail.com>
2022-07-08 15:08:11 +0800

    Update map display.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import {Paper,Typography,useMediaQuery} from "@material-ui/core";
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from "@material-ui/lab/Rating";
+import Rating from '@material-ui/lab/Rating';
 
 import mapStyles from './mapStyles.js';
 import useStyles from './styles.js';
 
-
-const Map = ({ setCoordinate, setBounds, coordinates, places, setChildChlicked, weatherData }) => {
+const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
+  const matches = useMediaQuery('(min-width:600px)');
   const classes = useStyles();
-  const isDesktop=useMediaQuery("(min-width:600px)");
 
   return (
     <div className={classes.mapContainer}>
-      <GoogleMapReact 
-      bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }} 
-      defaultCenter={coordinates} 
-      center={coordinates} 
-      defaultZoom={14}
-      margin={[50,50,50,50]}
-      options={{disableDefaultUI: true, zoomControl: true, styles: mapStyles}}
-      onChange={(e)=>{
-        setCoordinate({lat: e.center.lat, lng:e.center.lng});
-        setBounds({ne:e.marginBounds.ne, sw:e.marginBounds.sw})
-      }}
-      onChildClick={(child)=>setChildChlicked(child)}
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+        onChange={(e) => {
+          setCoords({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => setChildClicked(child)}
       >
-        {places?.map((place,i)=>{
-          <div className={classes.marketContainer}
-          lat={Number(place.latitude)}
-          lng={Number(place.longitude)}
-          key={i}
+        {places.length && places.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
           >
-            {!isDesktop?(
-              <LocationOnOutlinedIcon color="primary" fontSize="large"/>
-              ):(
+            {!matches
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
                 <Paper elevation={3} className={classes.paper}>
                   <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
                   <img
@@ -46,15 +46,15 @@ const Map = ({ setCoordinate, setBounds, coordinates, places, setChildChlicked, 
                 </Paper>
               )}
           </div>
-        })}
-        {weatherData?.list?.length && weatherData.list.map((data,i)=>{
+        ))}
+        {weatherData?.list?.length && weatherData.list.map((data, i) => (
           <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
-            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="90px"/>
+            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="90px" />
           </div>
-        })}
+        ))}
       </GoogleMapReact>
     </div>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
